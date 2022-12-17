9de44ffba1c30f60d82cc509df6972e7e761451a
prabin <kdlprabin444@gmail.com>
2022-12-17 09:17:58 +0545

    get method configured

--- client/src/routes/Add.js
+++ client/src/routes/Add.js
@@ -1,24 +1,74 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
+import { useState, useEffect } from "react";
 function Add() {
-  const handleSubmit = () => {
-    alert("The To Do has been submitted.");
+  const data = {
+    title: "",
+    description: "",
+    happened_date: "",
+    if_done: "",
   };
+  const [formData, setFormData] = useState(data);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (isSubmit) {
+      // axios.post("http://localhost:3001/api/post", data);
+      axios
+        .post("https://jsonplaceholder.typicode.com/posts",{
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        })
+        .then((response) => console.log(response.data));
+    }
+    setIsSubmit(false);
+  });
   return (
     <div className="add-items-form">
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Title</Form.Label>
-          <Form.Control type="text" placeholder="Enter Title" />
+          <Form.Control
+            type="text"
+            placeholder="Enter Title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Description</Form.Label>
-          <Form.Control as="textarea" row={5} placeholder="Description...." />
+          <Form.Control
+            as="textarea"
+            row={5}
+            placeholder="Description...."
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Select Date</Form.Label>
-          <Form.Control type="date" placeholder="Completed Date" />
+          <Form.Control
+            type="date"
+            placeholder="Completed Date"
+            name="happened_data"
+            value={formData.happened_date}
+            onChange={handleChange}
+          />
         </Form.Group>
         <Button variant="primary" type="button" onClick={handleSubmit}>
           Submit
